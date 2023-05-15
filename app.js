d7aeaa3f34c3ff2989cd3ef58038a1a488809e6b
Mateus Dinotto <dinottom8@gmail.com>
2023-05-15 17:28:32 -0300

    html, css e as animações mais básicas das três primeiras páginas prontas

--- app.js
+++ app.js
@@ -10,16 +10,19 @@ function chamarPaginaUm () {
     document.getElementById("divFotoEForma").style.display = "block";
     document.getElementById("divApresentacaoPaginaUm").style.display = "block";
     document.getElementById("mainPaginaDois").style.display = "none";
+    document.getElementById("mainPaginaTres").style.display = "none";
 }
 
 function chamarPaginaDois () {
     document.getElementById("divFotoEForma").style.display = "none";
     document.getElementById("divApresentacaoPaginaUm").style.display = "none";
     document.getElementById("mainPaginaDois").style.display = "flex";
+    document.getElementById("mainPaginaTres").style.display = "none";
 }
 
 function chamarPaginaTres () {
     document.getElementById("divFotoEForma").style.display = "none";
     document.getElementById("divApresentacaoPaginaUm").style.display = "none";
     document.getElementById("mainPaginaDois").style.display = "none";
+    document.getElementById("mainPaginaTres").style.display = "block";
 }
