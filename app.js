e62fed4e8dc3f75f220040282fd312853735c06e
Mateus Dinotto <dinottom8@gmail.com>
2023-05-15 16:59:37 -0300

    Feita parte de html e css da página 2. Ainda não foram inseridas as animações.

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ botaoPaginaCompetencias.addEventListener("click", chamarPaginaDois);
 function chamarPaginaUm () {
     document.getElementById("divFotoEForma").style.display = "block";
     document.getElementById("divApresentacaoPaginaUm").style.display = "block";
+    document.getElementById("mainPaginaDois").style.display = "none";
 }
 
 function chamarPaginaDois () {
     document.getElementById("divFotoEForma").style.display = "none";
     document.getElementById("divApresentacaoPaginaUm").style.display = "none";
+    document.getElementById("mainPaginaDois").style.display = "flex";
 }
