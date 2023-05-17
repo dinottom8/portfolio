81bbab6ab81f8f4d9e58910137b92fd95541e6d5
Mateus Dinotto <dinottom8@gmail.com>
2023-05-17 19:25:46 -0300

    inversão de cor da nav bar quando seleciona um botão

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ function chamarPaginaUm () {
     document.getElementById("mainPaginaTres").style.display = "none";
     document.getElementById("mainPaginaQuatro").style.display = "none";
     document.getElementById("mainPaginaCinco").style.display = "none";
+    document.getElementById("botaoPaginaInicial").style.backgroundColor = "#DE95ED";
+    document.getElementById("botaoPaginaCompetencias").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaExperiencias").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaProjetos").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaContato").style.backgroundColor = "#811498";
+
+    document.getElementById("imagem1").src = "images/person-circle2.svg";
+    document.getElementById("imagem2").src = "images/file-text.svg";
+    document.getElementById("imagem3").src = "images/briefcase.svg";
+    document.getElementById("imagem4").src = "images/columns-gap.svg";
+    document.getElementById("imagem5").src = "images/envelope.svg";
 }
 
 function chamarPaginaDois () {
@@ -24,6 +35,17 @@ function chamarPaginaDois () {
     document.getElementById("mainPaginaTres").style.display = "none";
     document.getElementById("mainPaginaDois").style.display = "flex";
     document.getElementById("mainPaginaCinco").style.display = "none";
+    document.getElementById("botaoPaginaInicial").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaCompetencias").style.backgroundColor = "#DE95ED";
+    document.getElementById("botaoPaginaExperiencias").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaProjetos").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaContato").style.backgroundColor = "#811498";
+
+    document.getElementById("imagem1").src = "images/person-circle.svg";
+    document.getElementById("imagem2").src = "images/file-text2.svg";
+    document.getElementById("imagem3").src = "images/briefcase.svg";
+    document.getElementById("imagem4").src = "images/columns-gap.svg";
+    document.getElementById("imagem5").src = "images/envelope.svg";
 }
 
 function chamarPaginaTres () {
@@ -32,6 +54,17 @@ function chamarPaginaTres () {
     document.getElementById("mainPaginaQuatro").style.display = "none";
     document.getElementById("mainPaginaTres").style.display = "block";
     document.getElementById("mainPaginaCinco").style.display = "none";
+    document.getElementById("botaoPaginaInicial").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaCompetencias").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaExperiencias").style.backgroundColor = "#DE95ED";
+    document.getElementById("botaoPaginaProjetos").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaContato").style.backgroundColor = "#811498";
+
+    document.getElementById("imagem1").src = "images/person-circle.svg";
+    document.getElementById("imagem2").src = "images/file-text.svg";
+    document.getElementById("imagem3").src = "images/briefcase2.svg";
+    document.getElementById("imagem4").src = "images/columns-gap.svg";
+    document.getElementById("imagem5").src = "images/envelope.svg";
 }
 
 function chamarPaginaQuatro () {
@@ -40,6 +73,17 @@ function chamarPaginaQuatro () {
     document.getElementById("mainPaginaTres").style.display = "none";
     document.getElementById("mainPaginaQuatro").style.display = "flex";
     document.getElementById("mainPaginaCinco").style.display = "none";
+    document.getElementById("botaoPaginaInicial").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaCompetencias").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaExperiencias").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaProjetos").style.backgroundColor = "#DE95ED";
+    document.getElementById("botaoPaginaContato").style.backgroundColor = "#811498";
+
+    document.getElementById("imagem1").src = "images/person-circle.svg";
+    document.getElementById("imagem2").src = "images/file-text.svg";
+    document.getElementById("imagem3").src = "images/briefcase.svg";
+    document.getElementById("imagem4").src = "images/columns-gap2.svg";
+    document.getElementById("imagem5").src = "images/envelope.svg";
 }
 
 function chamarPaginaCinco () {
@@ -48,4 +92,15 @@ function chamarPaginaCinco () {
     document.getElementById("mainPaginaTres").style.display = "none";
     document.getElementById("mainPaginaQuatro").style.display = "none";
     document.getElementById("mainPaginaCinco").style.display = "flex";
+    document.getElementById("botaoPaginaInicial").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaCompetencias").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaExperiencias").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaProjetos").style.backgroundColor = "#811498";
+    document.getElementById("botaoPaginaContato").style.backgroundColor = "#DE95ED";
+
+    document.getElementById("imagem1").src = "images/person-circle.svg";
+    document.getElementById("imagem2").src = "images/file-text.svg";
+    document.getElementById("imagem3").src = "images/briefcase.svg";
+    document.getElementById("imagem4").src = "images/columns-gap.svg";
+    document.getElementById("imagem5").src = "images/envelope2.svg";
 }
