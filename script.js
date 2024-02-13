2a22ccd3cbabc6d9aca732d859dcc5643305e115
teuteu <dinottom8@gmail.com>
2024-02-13 18:06:44 -0300

    fixed the window handles

--- script.js
+++ script.js
@@ -4,13 +4,34 @@ window_handles.forEach((el) => {
     el.addEventListener("click", () => selectWindow(el));
 });
 
-for (let i = window_handles.length - 1, u = window_handles.length; i >= 0; i--, u++) {
-    window_handles[i].style.zIndex = u
+fixzIndex();
+
+function fixzIndex() {
+    // set the correct zIndex for the window handles    
+    for ( let i = 0, countzIndex = window_handles.length ; i < window_handles.length ; i++ , countzIndex-- ) {
+        window_handles[i].style.zIndex = countzIndex;
+    }
+
 }
 
+
 function selectWindow(el) {
+
+    
+    if (el != window_handles[2]) {
+        fixzIndex();
+    } else {
+        window_handles[0].style.zIndex = 1;
+        window_handles[1].style.zIndex = 2;
+    }
+
+    // remove all active-window-handle classes
     window_handles.map((elem)=>{
         elem.classList.remove("active-window-handle");
     })
+
+    // add the class just for the right one that is selected
     el.classList.add("active-window-handle");
+    el.style.zIndex = 3;
+
 }
